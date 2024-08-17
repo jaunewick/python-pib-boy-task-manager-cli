a37f836abae0eac3e0c71ee8f835b0fe1938ad42
Daniel Giao <daniel.giao@icloud.com>
2024-08-17 05:34:55 -0400

    Added read tasks

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pickle
 from datetime import datetime
 
 tasks = []
-TASK_FILE =  task.pkl
+TASK_FILE =  tasks.pkl
 
 class TaskManager():
     def __init__(self, title, created_at, is_completed=False):
@@ -17,10 +17,16 @@ def add_task():
     created_at = datetime.now().strftime("%d/%m %H:%M")
     task = TaskManager(title, created_at)
     tasks.append(task)
-
     # TODO Save to pickle file
 
 # Read
+def print_all_tasks():
+    print("+----+-------------------------------------+--------------+-------------+")
+    print("| ID |            Task Title               |  Created at  |  Completed  |")
+    print("+----+-------------------------------------+--------------+-------------+")
+    for i, task in enumerate(tasks):
+        print(f"| {i + 1:2} | {task.title:35} | {task.created_at:12} | {'✅' if task.is_completed else '❌':^11}|")
+    print("+----+-------------------------------------+--------------+-------------+")
 
 # Update
 
