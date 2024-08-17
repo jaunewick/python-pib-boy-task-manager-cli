85930a81ec6697c308a29fff04576a89d0a6f53f
Daniel Giao <daniel.giao@icloud.com>
2024-08-17 05:57:19 -0400

    Added update task

--- main.py
+++ main.py
@@ -6,28 +6,39 @@ tasks = []
 TASK_FILE =  tasks.pkl
 
 class TaskManager():
-    def __init__(self, title, created_at, is_completed=False):
+    def __init__(self, title, created_at, is_done=False):
         self.title = title
         self.created_at = created_at
-        self.is_completed = is_completed
+        self.is_done = is_done
 
 # Create
 def add_task():
     title = input("Type your task: ")
     created_at = datetime.now().strftime("%d/%m %H:%M")
+
     task = TaskManager(title, created_at)
     tasks.append(task)
     # TODO Save to pickle file
 
 # Read
 def print_all_tasks():
-    print("+----+-------------------------------------+--------------+-------------+")
-    print("| ID |            Task Title               |  Created at  |  Completed  |")
-    print("+----+-------------------------------------+--------------+-------------+")
+    print("+------+-------------------------------------+--------------+--------+")
+    print("|  ID  |            Task Title               |  Created at  |  Done  |")
+    print("+------+-------------------------------------+--------------+--------+")
     for i, task in enumerate(tasks):
-        print(f"| {i + 1:2} | {task.title:35} | {task.created_at:12} | {'✅' if task.is_completed else '❌':^11}|")
-    print("+----+-------------------------------------+--------------+-------------+")
+        print(f"| {i + 1:4} | {task.title:35} | {task.created_at:12} | {'✅' if task.is_completed else '❌':6} |")
+    print("+------+-------------------------------------+--------------+--------+")
 
 # Update
+def update_task():
+    print_all_tasks()
+
+    id = int(input("Enter the ID of the task:"))
+
+    tasks[id].is_done = True
+    # TODO Save to pickle file
+
+
+
 
 # Delete
